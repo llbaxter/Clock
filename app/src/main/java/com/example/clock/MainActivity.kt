23c708953d0baa186e5af876ee47c5283ffa835f
llbaxter <llbaxter123@gmail.com>
2021-08-22 15:18:35 +0100

    changed sdk from 16 to 26
    changed due to library not being supported for earlier versions of android

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -2,13 +2,15 @@ package com.example.clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //test
+
     }
 }
