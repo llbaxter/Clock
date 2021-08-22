06d29003fcf84b52d7d3aebfe03150bd4453f2df
llbaxter <llbaxter123@gmail.com>
2021-08-22 16:13:22 +0100

    time, date added
    time, date added

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -10,6 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val dateNow = Calendar.getInstance().time.toString()
-        Log.d("msg", dateNow)
+        val dayNow = dateNow.substring(0,3)
+        val monthNow = dateNow.substring(4,7)
+        val yearNow = dateNow.substring(8,10)
+        val timeNow = dateNow.substring(11, 20)
+
+        Log.d("msg", dayNow)
+        Log.d("msg", monthNow)
+        Log.d("msg", yearNow)
+        Log.d("msg", timeNow)
     }
 }
