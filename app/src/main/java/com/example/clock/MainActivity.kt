e8a9bd8896d10b21e10cde3e8b245f005b6d5bf7
llbaxter <llbaxter123@gmail.com>
2021-08-23 05:40:03 +0100

    added text clock, removed title bar
    added text clock and styling to main activity and removed the title bar

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -3,21 +3,15 @@ package com.example.clock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
+import java.time.Clock
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
-        val dateNow = Calendar.getInstance().time.toString()
-        val dayNow = dateNow.substring(0,3)
-        val monthNow = dateNow.substring(4,7)
-        val yearNow = dateNow.substring(8,10)
-        val timeNow = dateNow.substring(11, 20)
-
-        Log.d("msg", dayNow)
-        Log.d("msg", monthNow)
-        Log.d("msg", yearNow)
-        Log.d("msg", timeNow)
     }
 }
